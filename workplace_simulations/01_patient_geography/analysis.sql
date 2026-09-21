972b70828bed6eaa56e60f5ed5267ea6e53fcd23
EB <elizabethjuliaball@gmail.com>
2026-09-21 11:12:39 -0400

    Add starter query for geography simulation

---
+++ workplace_simulations/01_patient_geography/analysis.sql
@@ -0,0 +1,16 @@
+-- WORKPLACE SIMULATION 01
+-- Stakeholder question:
+-- How many unique tested patients are present in each province?
+--
+-- Build the query below.
+--
+-- Expected concepts:
+-- SELECT
+-- COUNT(DISTINCT ...)
+-- FROM
+-- JOIN
+-- ON
+-- GROUP BY
+-- ORDER BY
+
+
