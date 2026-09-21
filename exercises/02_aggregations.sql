c7a49f0320619b17e953eb116be214de52acad7c
EB <elizabethjuliaball@gmail.com>
2026-09-21 09:45:51 -0400

    Add aggregation SQL exercises

---
+++ exercises/02_aggregations.sql
@@ -0,0 +1,20 @@
+-- Lesson 02: COUNT, SUM, AVG, MIN, MAX, GROUP BY
+
+-- 1. How many customers are in the database?
+
+
+-- 2. What is the average product price?
+
+
+-- 3. How many customers are in each province?
+
+
+-- 4. What is the most expensive product?
+
+
+-- 5. How many orders have each status?
+
+
+-- CHALLENGE
+-- What is the total dollar value of each order?
+-- Hint: quantity * unit_price
