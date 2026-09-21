0ce5ffdbf36a685d78dad122c7de4125e5208d36
EB <elizabethjuliaball@gmail.com>
2026-09-21 09:45:48 -0400

    Add beginner SQL exercises

---
+++ exercises/01_basics.sql
@@ -0,0 +1,37 @@
+-- BUILDING SQL COURSE
+-- Lesson 01: SELECT, WHERE, ORDER BY, LIMIT
+--
+-- Run:
+-- sqlite3 practice.db
+--
+-- Then:
+-- .headers on
+-- .mode column
+
+-- 1. Return every customer.
+SELECT *
+FROM customers;
+
+-- 2. TODO: Return only first_name, last_name, and city.
+
+
+-- 3. TODO: Find all customers from Ontario (ON).
+
+
+-- 4. TODO: Find products that cost more than $50.
+
+
+-- 5. TODO: Sort products from highest price to lowest price.
+
+
+-- 6. TODO: Return the three most expensive products.
+
+
+-- 7. TODO: Find all completed orders.
+
+
+-- 8. TODO: Find orders placed on or after July 1, 2026.
+
+
+-- CHALLENGE
+-- Which customers live in Hamilton or Toronto?
